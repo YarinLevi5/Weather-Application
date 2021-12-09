18984c81b9a7f009391d53d86a03429c7fc74823
yarin <yarin066@gmail.com>
2021-12-09 16:00:57 +0200

    JSON replaces the old options

--- Cweather.js
+++ Cweather.js
@@ -3,12 +3,20 @@ $(function () {
 });
 
 const container = $('.container');
-container.prepend('<h1 style=color:purple>Weather App</h1>');
+container.prepend('<h1 style=color:purple>Weather app of Israel cities </h1>');
+let list = $('#options');
+let option = $('option');
+
+$.get('il.cities.json', data => {
+    console.log(data);
+    $.each(data, function (i, ilCity) {
+        let cityToString = JSON.stringify(ilCity.city)
+        list.append(`<option>${JSON.parse(cityToString)}</option>`);
+    });
+});
 
 function weatherFromCities(cb) {
-    let list = $('#options');
     $(list).change(function (city) {
-        //debugger;
         city = this.value;
         $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=590750d4091653d153864c26c2670af8`, weatherData => {
             cb(weatherData);
